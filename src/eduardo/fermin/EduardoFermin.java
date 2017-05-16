dfaabd27920a8b4e92c3a76d70f78e1d58f895b1
eduardofermin <efermin57@gmail.com>
2017-05-15 19:44:20 -0500

    Update EduardoFermin.java

--- src/eduardo/fermin/EduardoFermin.java
+++ src/eduardo/fermin/EduardoFermin.java
@@ -16,7 +16,7 @@ public class EduardoFermin {
      */
     public static void main(String[] args) {
         
-        System.out.println("Hola mundo");
+        System.out.println("Hola mundo cruel");
         
     }
     
